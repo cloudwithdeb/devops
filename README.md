866026a54314e52828eeabc8ed7d1c3314a939a2
Owusu Bright Debrah <brightdebrah97@gmail.com>
2023-02-28 08:12:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Title
+Azure Kubernetes Complete Guide
---
+++ script.sh
@@ -0,0 +1,18 @@
+# update kubernetes cluster to integrate azure container registry to it
+CONTAINER_RG='sibac'
+AKS_NAME='Siba'
+AKS_RG='Siba'
+
+az aks update -n $AKS_NAME -g $AKS_RG --attach-acr $CONTAINER_RG
+az aks update -n $AKS_NAME -g $AKS_RG --detach-acr $CONTAINER_RG
+
+# Build docker image using acr
+az acr build  --image spring/app:v1 --registry $REGISTRY_NAME
+
+kubectl run hw --image=sibac.azurecr.io/hello-worl:v1
+
+az acr import -n container-rg \
+--source docker.io/library/nginx:latest \
+--image nginx:v1
+
+az acr repository list --name container-rg -o table
